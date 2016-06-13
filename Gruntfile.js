2e03c95174576014625dcd0663293e10ed80694b
Tal Ater <tal@talater.com>
2016-06-13 16:57:08 +0300

    Added grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,17 @@ module.exports = function(grunt) {
           'dist/progressive-ui-kitt.min.js': ['src/progressive-ui-kitt.js']
         }
       }
+    },
+    watch: {
+      files: ['src/progressive-ui-kitt.js', '!**/node_modules/**'],
+      tasks: ['default']
     }
   });
 
   // Load NPM Tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint', 'uglify']);
